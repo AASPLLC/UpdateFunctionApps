8379eddf69ac86611d7e1595b8893e980e99ee08
Wayne Harper <Wayne@aasp.com>
2023-01-23 12:23:49 -0600

    Add project files.

---
+++ UpdateFunctionApps/InstallConfig.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace UpdateFunctionApps
+{
+    partial class InstallConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectSubBTN = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.smsFunctionCB = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.selectSMSBTN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.whatsAppFunctionCB = new System.Windows.Forms.ComboBox();
+            this.selectWhatsAppBTN = new System.Windows.Forms.Button();
+            this.outputRT = new System.Windows.Forms.RichTextBox();
+            this.selectCosmosBTN = new System.Windows.Forms.Button();
+            this.selectCosmosCB = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectSubBTN
+            // 
+            this.selectSubBTN.Location = new System.Drawing.Point(430, 27);
+            this.selectSubBTN.Name = "selectSubBTN";
+            this.selectSubBTN.Size = new System.Drawing.Size(75, 23);
+            this.selectSubBTN.TabIndex = 0;
+            this.selectSubBTN.Text = "Select";
+            this.selectSubBTN.UseVisualStyleBackColor = true;
+            this.selectSubBTN.Click += new System.EventHandler(this.SelectSubscription_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(411, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Subscription for Update";
+            // 
+            // smsFunctionCB
+            // 
+            this.smsFunctionCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.smsFunctionCB.Enabled = false;
+            this.smsFunctionCB.FormattingEnabled = true;
+            this.smsFunctionCB.Location = new System.Drawing.Point(13, 71);
+            this.smsFunctionCB.Name = "smsFunctionCB";
+            this.smsFunctionCB.Size = new System.Drawing.Size(411, 23);
+            this.smsFunctionCB.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select SMS Function App:";
+            // 
+            // selectSMSBTN
+            // 
+            this.selectSMSBTN.Enabled = false;
+            this.selectSMSBTN.Location = new System.Drawing.Point(430, 71);
+            this.selectSMSBTN.Name = "selectSMSBTN";
+            this.selectSMSBTN.Size = new System.Drawing.Size(75, 23);
+            this.selectSMSBTN.TabIndex = 8;
+            this.selectSMSBTN.Text = "Select";
+            this.selectSMSBTN.UseVisualStyleBackColor = true;
+            this.selectSMSBTN.Click += new System.EventHandler(this.SelectSMS_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select WhatsApp Function App:";
+            // 
+            // whatsAppFunctionCB
+            // 
+            this.whatsAppFunctionCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.whatsAppFunctionCB.Enabled = false;
+            this.whatsAppFunctionCB.FormattingEnabled = true;
+            this.whatsAppFunctionCB.Location = new System.Drawing.Point(13, 115);
+            this.whatsAppFunctionCB.Name = "whatsAppFunctionCB";
+            this.whatsAppFunctionCB.Size = new System.Drawing.Size(411, 23);
+            this.whatsAppFunctionCB.TabIndex = 10;
+            // 
+            // selectWhatsAppBTN
+            // 
+            this.selectWhatsAppBTN.Enabled = false;
+            this.selectWhatsAppBTN.Location = new System.Drawing.Point(430, 115);
+            this.selectWhatsAppBTN.Name = "selectWhatsAppBTN";
+            this.selectWhatsAppBTN.Size = new System.Drawing.Size(75, 23);
+            this.selectWhatsAppBTN.TabIndex = 11;
+            this.selectWhatsAppBTN.Text = "Select";
+            this.selectWhatsAppBTN.UseVisualStyleBackColor = true;
+            this.selectWhatsAppBTN.Click += new System.EventHandler(this.SelectWhatsApp_Click);
+            // 
+            // outputRT
+            // 
+            this.outputRT.Location = new System.Drawing.Point(10, 187);
+            this.outputRT.Name = "outputRT";
+            this.outputRT.Size = new System.Drawing.Size(495, 129);
+            this.outputRT.TabIndex = 12;
+            this.outputRT.Text = "";
+            // 
+            // selectCosmosBTN
+            // 
+            this.selectCosmosBTN.Enabled = false;
+            this.selectCosmosBTN.Location = new System.Drawing.Point(430, 157);
+            this.selectCosmosBTN.Name = "selectCosmosBTN";
+            this.selectCosmosBTN.Size = new System.Drawing.Size(75, 24);
+            this.selectCosmosBTN.TabIndex = 15;
+            this.selectCosmosBTN.Text = "Start";
+            this.selectCosmosBTN.UseVisualStyleBackColor = true;
+            this.selectCosmosBTN.Click += new System.EventHandler(this.SelectCosmos_Click);
+            // 
+            // selectCosmosCB
+            // 
+            this.selectCosmosCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.selectCosmosCB.Enabled = false;
+            this.selectCosmosCB.FormattingEnabled = true;
+            this.selectCosmosCB.Location = new System.Drawing.Point(13, 158);
+            this.selectCosmosCB.Name = "selectCosmosCB";
+            this.selectCosmosCB.Size = new System.Drawing.Size(411, 23);
+            this.selectCosmosCB.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Select Cosmos REST Function App:";
+            // 
+            // InstallConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 328);
+            this.Controls.Add(this.selectCosmosBTN);
+            this.Controls.Add(this.selectCosmosCB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outputRT);
+            this.Controls.Add(this.selectWhatsAppBTN);
+            this.Controls.Add(this.whatsAppFunctionCB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.selectSMSBTN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.smsFunctionCB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.selectSubBTN);
+            this.Name = "InstallConfig";
+            this.Text = "Check for updates";
+            this.Load += new System.EventHandler(this.InstallConfig_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button selectSubBTN;
+        private ComboBox comboBox1;
+        private Label label1;
+        private ComboBox smsFunctionCB;
+        private Label label3;
+        private Button selectSMSBTN;
+        private Label label2;
+        private ComboBox whatsAppFunctionCB;
+        private Button selectWhatsAppBTN;
+        private RichTextBox outputRT;
+        private Button selectCosmosBTN;
+        private ComboBox selectCosmosCB;
+        private Label label4;
+    }
+}
---
+++ UpdateFunctionApps/InstallConfig.cs
@@ -0,0 +1,195 @@
+﻿using Azure.ResourceManager.Resources;
+using System.Net.Http.Headers;
+using AASPGlobalLibrary;
+using Azure.ResourceManager;
+using Azure;
+using Azure.ResourceManager.AppService;
+using System.Text.Json;
+
+namespace UpdateFunctionApps
+{
+    public partial class InstallConfig : Form
+    {
+        readonly ArmClient client = TokenHandler.CreateArmClient();
+
+        public SubscriptionResource? SelectedSubscription;
+        public ResourceGroupResource? SelectedGroup;
+
+        JSONCustomSettings json = new();
+
+        public InstallConfig()
+        {
+            InitializeComponent();
+
+            _ = new SetConsoleOutput(outputRT);
+        }
+
+        List<SubscriptionResource> subids = new();
+
+        public (List<string>, List<SubscriptionResource>) SetupSubscriptionName()
+        {
+            List<string> names = new();
+            List<SubscriptionResource> ids = new();
+            SubscriptionCollection subscriptions = client.GetSubscriptions();
+            List<SubscriptionResource> subs = subscriptions.GetAll().ToList();
+            for (int i = 0; i < subs.Count; i++)
+            {
+                names.Add(subs[i].Data.DisplayName);
+                ids.Add(subs[i]);
+            }
+            return (names, ids);
+        }
+
+        public static (List<string>, List<ResourceGroupResource>) SetupResourceName(SubscriptionResource sr)
+        {
+            List<string> names = new();
+            List<ResourceGroupResource> ids = new();
+            ResourceGroupCollection resourceGroups = sr.GetResourceGroups();
+            foreach (var item in resourceGroups)
+            {
+                names.Add(item.Data.Name);
+                ids.Add(item);
+            }
+            return (names, ids);
+        }
+
+        private async void InstallConfig_Load(object sender, EventArgs e)
+        {
+#pragma warning disable CS8601
+            json = JsonSerializer.Deserialize<JSONCustomSettings>(await File.ReadAllTextAsync(Environment.CurrentDirectory + "/CustomSettings.json"));
+#pragma warning restore CS8601
+
+            (var names, subids) = SetupSubscriptionName();
+            comboBox1.Items.AddRange(names.ToArray());
+            comboBox1.SelectedIndex = 0;
+
+            var disco = "https://globaldisco.crm.dynamics.com/api/discovery/v1.0/Instances";
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await TokenHandler.GetGlobalDynamicsImpersonationToken());
+            HttpRequestMessage request = new(new("GET"), disco);
+            _ = await httpClient.SendAsync(request);
+        }
+
+        public class JSONCustomSettings
+        {
+            public string? SMSFunctionZipDeployPath { get; set; }
+            public string? WhatsAppFunctionZipDeployPath { get; set; }
+            public string? CosmosRESTZipDeployPath { get; set; }
+        }
+
+        private async void SelectSubscription_Click(object sender, EventArgs e)
+        {
+            SelectedSubscription = subids[comboBox1.SelectedIndex];
+            SelectedGroup = await SelectedSubscription.GetResourceGroups().GetAsync("SMSAndWhatsAppResourceGroup");
+
+
+            outputRT.Text = "Getting Functions...";
+            List<WebSiteResource> sites = SelectedGroup.GetWebSites().GetAll().ToList();
+            for (int i = 0; i < sites.Count; i++)
+            {
+                smsFunctionCB.Items.Add(sites[i].Data.Name);
+                whatsAppFunctionCB.Items.Add(sites[i].Data.Name);
+                selectCosmosCB.Items.Add(sites[i].Data.Name);
+            }
+            for (int i = 0; i < smsFunctionCB.Items.Count; i++)
+            {
+                if (smsFunctionCB.Items[i].ToString().EndsWith("SMSApp"))
+                {
+                    smsFunctionCB.SelectedIndex = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < whatsAppFunctionCB.Items.Count; i++)
+            {
+                if (whatsAppFunctionCB.Items[i].ToString().EndsWith("WhatsApp"))
+                {
+                    whatsAppFunctionCB.SelectedIndex = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < selectCosmosCB.Items.Count; i++)
+            {
+                if (selectCosmosCB.Items[i].ToString().EndsWith("CosmosREST"))
+                {
+                    selectCosmosCB.SelectedIndex = i;
+                    break;
+                }
+            }
+            outputRT.Text += Environment.NewLine + "Functions found are now listed.";
+
+            selectSubBTN.Enabled = false;
+            comboBox1.Enabled = false;
+            subids.Clear();
+            selectSMSBTN.Enabled = true;
+            smsFunctionCB.Enabled = true;
+        }
+
+        private void SelectSMS_Click(object sender, EventArgs e)
+        {
+            selectSMSBTN.Enabled = false;
+            smsFunctionCB.Enabled = false;
+            selectWhatsAppBTN.Enabled = true;
+            whatsAppFunctionCB.Enabled = true;
+        }
+
+        private void SelectWhatsApp_Click(object sender, EventArgs e)
+        {
+            selectSubBTN.Enabled = false;
+            comboBox1.Enabled = false;
+            selectWhatsAppBTN.Enabled = false;
+            whatsAppFunctionCB.Enabled = false;
+            selectSMSBTN.Enabled = false;
+            smsFunctionCB.Enabled = false;
+            selectCosmosBTN.Enabled = true;
+            selectCosmosCB.Enabled = true;
+        }
+
+        private async void SelectCosmos_Click(object sender, EventArgs e)
+        {
+            var results = MessageBox.Show("Warning: This will stop all SMS and WhatsApp messages temporarily." + Environment.NewLine + "Only do this when active users are at a low usage rate.", "", MessageBoxButtons.OKCancel);
+            if (results == DialogResult.OK)
+            {
+                selectSubBTN.Enabled = false;
+                comboBox1.Enabled = false;
+                selectWhatsAppBTN.Enabled = false;
+                whatsAppFunctionCB.Enabled = false;
+                selectSMSBTN.Enabled = false;
+                smsFunctionCB.Enabled = false;
+                selectCosmosBTN.Enabled = false;
+                selectCosmosCB.Enabled = false;
+                outputRT.Text = "Getting Ready...";
+#pragma warning disable CS8604
+                var smsSiteResource = (await SelectedGroup.GetWebSiteAsync(smsFunctionCB.Text)).Value;
+                PublishingUserResource user = (await smsSiteResource.GetPublishingCredentialsAsync(WaitUntil.Completed)).Value;
+                outputRT.Text += Environment.NewLine + "Zip Deploy Started for SMS Function";
+                await FunctionAppHandler.ZipDeploy(user, json.SMSFunctionZipDeployPath);
+                outputRT.Text += Environment.NewLine + "Zip Deploy for SMS Function finished";
+
+                var whatsAppSiteResource = (await SelectedGroup.GetWebSiteAsync(whatsAppFunctionCB.Text)).Value;
+                user = (await whatsAppSiteResource.GetPublishingCredentialsAsync(WaitUntil.Completed)).Value;
+                outputRT.Text += Environment.NewLine + "Zip Deploy Started for WhatsApp Function";
+                await FunctionAppHandler.ZipDeploy(user, json.WhatsAppFunctionZipDeployPath);
+                outputRT.Text += Environment.NewLine + "Zip Deploy for WhatsApp Function finished";
+
+                if (selectCosmosCB.Text != "")
+                {
+                    var cosmosSiteResource = (await SelectedGroup.GetWebSiteAsync(selectCosmosCB.Text)).Value;
+                    user = (await cosmosSiteResource.GetPublishingCredentialsAsync(WaitUntil.Completed)).Value;
+                    outputRT.Text += Environment.NewLine + "Zip Deploy Started for Cosmos REST API Function";
+                    await FunctionAppHandler.ZipDeploy(user, json.CosmosRESTZipDeployPath);
+                    outputRT.Text += Environment.NewLine + "Zip Deploy for Cosmos REST API Function finished";
+                }
+#pragma warning restore CS8604
+                selectSubBTN.Enabled = true;
+                comboBox1.Enabled = true;
+                selectWhatsAppBTN.Enabled = true;
+                whatsAppFunctionCB.Enabled = true;
+                selectSMSBTN.Enabled = true;
+                smsFunctionCB.Enabled = true;
+                selectCosmosBTN.Enabled = true;
+                selectCosmosCB.Enabled = true;
+            }
+        }
+    }
+}
